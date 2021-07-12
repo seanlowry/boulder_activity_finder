cea672d18ce92599339effc24d5860701d46ffa3
seanlowry <selo4751@colorado.edu>
2021-07-11 22:02:31 -0600

    registration/server additions

--- All_Project_Code/Components/init_data/create.sql
+++ All_Project_Code/Components/init_data/create.sql
@@ -1,56 +1,53 @@
 
-DROP TABLE IF EXISTS user_details;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS activities;
 
-CREATE TABLE IF NOT EXISTS user_details(
+CREATE TABLE IF NOT EXISTS users(
   user_id SERIAL PRIMARY KEY,
-  firstName VARCHAR(30),
-  lastName VARCHAR(30),
+  firstname VARCHAR(30),
+  lastname VARCHAR(30),
+  username VARCHAR(30) UNIQUE,
   email VARCHAR(30) UNIQUE,
-  user_password VARCHAR(20) NOT NULL,
-  
+  user_password VARCHAR(20) NOT NULL
 );
 
-
-INSERT INTO user_details(user_id, firstName, lastName, email, user_password)
-VALUES(1,'admin','admin', 'admin@123.com', 'admin');
-
-/*
-*/
+INSERT INTO users (firstName, lastName, username, email, user_password)
+VALUES ('admin','admin','admin','admin@123.com','admin'),
+('Sean', 'Lowry', 'selo', 'selo4751@colorado.edu', 'password'),
+('David', 'Farrow', 'dafa', 'dafa9718@colorado.edu', 'password'),
+('Xiang', 'Chen', 'xich', 'xich4932@colorado.edu', 'password');
 
 CREATE TABLE IF NOT EXISTS posts(
   post_id SERIAL PRIMARY KEY,
-  author INT NOT NULL,
+  author_id INT NOT NULL,
   title VARCHAR(50),
   img_src TEXT,
   summary VARCHAR(255),
   full_desc TEXT,
-  FOREIGN KEY (author)
-    REFERENCES user_details(user_id)
+  FOREIGN KEY (author_id)
+    REFERENCES users(user_id)
 );
-INSERT INTO  posts(post_id, author, title, summary, full_desc)
-VALUES(11,1,'Hiking?', 'Looking for someone interested in hiking', 'join us via XXXXXXXXX');
-INSERT INTO  posts(post_id, author, title, summary, full_desc)
-VALUES(12,1,'lost shoe', 'I lost my shoe during snowboarding', 'please contact me when you found it');
-INSERT INTO  posts(post_id, author, title, summary, full_desc)
-VALUES(13,1,'how much is the ticket for fishing?', 'fishing ticket price', 'Me and friends plan to go there this week');
-INSERT INTO  posts(post_id, author, title, summary, full_desc)
-VALUES(14,1,'Anyone want to do cs review with classmates', 'group now is 4/7', 'please send me xxxxx if you are interested in');
-INSERT INTO  posts(post_id, author, title, summary, full_desc)
-VALUES(15,1,'Will CS final cancelled due to the upcoming storm', 'Will CS final cancelled due to the upcoming storm', 'I really appreciate ');
+
+INSERT INTO  posts(author_id, title, summary, full_desc)
+VALUES (1, 'PostTitle', 'This is the brief description of the post', 'And this will be a longer description'),
+(2,'Hiking?', 'Looking for someone interested in hiking', 'join us via XXXXXXXXX'),
+(2,'lost shoe', 'I lost my shoe during snowboarding', 'please contact me when you found it'),
+(3,'how much is the ticket for fishing?', '', 'Me and friends plan to go there this week'),
+(3,'Any party tonight?', '', ''),
+(4,'Will CS final cancelled due to the upcoming storm', '', 'I really appreciate ');
 
 
 CREATE TABLE IF NOT EXISTS messages(
   message_id SERIAL PRIMARY KEY,
-  author INT,
-  recipient INT,
+  author_id INT,
+  recipient_id INT,
   body TEXT,
-  FOREIGN KEY (author)
-    REFERENCES user_details(user_id),
-  FOREIGN KEY (recipient)
-    REFERENCES user_details(user_id)
+  FOREIGN KEY (author_id)
+    REFERENCES users(user_id),
+  FOREIGN KEY (recipient_id)
+    REFERENCES users(user_id)
 );
 
 /*
@@ -59,11 +56,13 @@ members implement with member id&id&id to record who join the activities
 
 CREATE TABLE IF NOT EXISTS activities(
   activity_id SERIAL PRIMARY KEY,
-  manager VARCHAR(30) NOT NULL,
-  members TEXT, 
+  manager_id INT NOT NULL,
+  member_ids INT[],
   activity_name VARCHAR(30) NOT NULL,
-  acitivity_time TIMESTAMPTZ
+  acitivity_time TIMESTAMPTZ,
+  FOREIGN KEY (manager_id)
+    REFERENCES users(user_id)
 );
 
-INSERT INTO activities(manager, activity_name, acitivity_time)
-VALUES('Admin', 'first activity', '2021-07-07 12:00:00');
+INSERT INTO activities(manager_id, activity_name, acitivity_time)
+VALUES(1, 'first activity', '2021-07-07 12:00:00');
--- All_Project_Code/Components/server.js
+++ All_Project_Code/Components/server.js
@@ -53,9 +53,9 @@ app.get('/home',function(req, res){
                 title: 'home',
                 allpost: ''
             })
-            
+
         })
-    
+
 });
 
 app.get('/post',function(req, res){
@@ -75,9 +75,9 @@ app.get('/post',function(req, res){
                 title: 'home',
                 allpost: ''
             })
-            
+
         })
-    
+
 });
 
 
@@ -100,20 +100,20 @@ app.post('/posting',function(req, res){
                 title: 'home',
                 allpost: ''
             })
-            
+
         })
-    
+
 });
 
 
 app.post('/login',function(req, res){
     console.log(req.body.inputEmail)
-    
+
     var email = req.body.inputEmail;
     var pass = req.body.inputPassword;
     //console.log(email)
    //console.log(pass)
-    var query1 = "SELECT user_password FROM user_details WHERE email = '"+email+"'"
+    var query1 = "SELECT user_password FROM users WHERE email = '"+email+"'"
     //console.log(query1)
     db.any(query1)
         .then(function(data){
@@ -131,18 +131,76 @@ app.post('/login',function(req, res){
                     log: ''
                 })
             }
-            
+
         })
         .catch(error =>{
             //request.flash(("error", error));
             res.render('pages/login',{
                 title: "login",
-                
+
             })
             console.log("error: ", error)
         });
-        
+
 });
+
+
+app.get('/registration', (req, res) => {
+	var existing_users = 'select username from users;';
+	db.any(existing_users)
+		.then(data => {
+			res.render("pages/registration", {
+				my_title: 'Register',
+				existing_users: data,
+				alert_msg: ''
+			})
+		})
+		.catch (err => {
+			console.log('ERROR:' + err);
+			res.render('pages/login', {
+				my_title: 'Login',
+				alert_msg: ''
+			})
+		})
+});
+
+
+app.post('/registration/new_user', (req, res) => {
+	var username = req.body.username;
+	var email = req.body.email;
+	var validateUserInfo = `select username, email from users where username= '${username}' or email= '${email}';`;
+	db.one(validateUserInfo)
+		.catch(err => {
+			console.log('ERROR:' + err);
+			res.render('pages/login', {
+				my_title: 'Login',
+				alert_msg: 'Error registering new user.'
+			})
+		})
+	var firstname = req.body.firstname;
+	var lastname = req.body.lastname;
+	var password = req.body.password;
+	var createUser = `insert into users (firstname, lastname, username, email, user_password) values ('${firstname}','${lastname}','${username}','${email}','${password}');`;
+	var getUserId = `select user_id from users where username = '${username}';`;
+	db.task('get-everything', task => {
+		task.any(createUser),
+		task.one(getUserId)
+	})
+		.then(data => {
+			res.render('pages/home', {
+				my_title: 'Home',
+				alert_msg: ''
+			})
+		})
+		.catch(err => {
+			console.log('ERROR:' + err);
+			res.render('pages/login', {
+				my_title: 'Login',
+				alert_msg: 'Error registering new user.'
+			})
+		})
+});
+
 
 app.listen(3000);
 console.log('3000 is the magic port');
